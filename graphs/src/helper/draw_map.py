fe2b2434d06b13f5a80c54b177ecd364c1cbbf49
abhijitbhatt <bhattacharya.abhi94@gmail.com>
2019-07-02 10:24:15 -0700

    Generate visualization only one path at a time

--- graphs/src/helper/draw_map.py
+++ graphs/src/helper/draw_map.py
@@ -2,13 +2,13 @@ import pydot_ng as pydot
 from helper.map40 import Map40
 import math
 
-def draw(road_map, paths, colors):
+def draw(road_map, path, colors, output_file):
     graph = pydot.Dot(graph_type='graph', rankdir="LR")
     nodes = {}
     edges = set()
 
     for k, v in road_map.intersections.items():
-        n = pydot.Node(k, shape="circle", style="filled", fillcolor=get_path_id(k, paths, colors), tooltip="(%f,%f)" % (v[0], v[1]))
+        n = pydot.Node(k, shape="circle", style="filled", fillcolor="cyan" if k not in path else colors[0], tooltip="(%f,%f)" % (v[0], v[1]))
         nodes[k] = n
         graph.add_node(n)
     for src, dst in enumerate(road_map.roads):
@@ -19,19 +19,15 @@ def draw(road_map, paths, colors):
                 edges.add((src,d))
                 edges.add((d,src))
                 found = False
-                for index, path in enumerate(paths):
-                    if src in path and d in path:
-                        graph.add_edge(pydot.Edge(nodes[src], nodes[d], style="bold", label="%0.2f" % distance, color=colors[index], tooltip="%0.2f" % distance))
-                        found = True
-                if not found:
-                    graph.add_edge(pydot.Edge(nodes[src], nodes[d], label="%0.2f" % distance, color="black", tooltip="%0.2f" % distance))
-    graph.write_svg("map40.svg")
-
-def get_path_id(node, paths, colors):
-    for index, path in enumerate(paths):
-        if node in path:
-            return colors[index]
-    return "cyan"
+                if src in path and d in path:
+                    color = colors[1]
+                    style="bold"
+                else:
+                    color = "black"
+                    style = "dashed"
+                graph.add_edge(pydot.Edge(nodes[src], nodes[d], style=style, label="%0.2f" % distance, color=color, tooltip="%0.2f" % distance))
+    graph.write_svg(output_file)
 
 if __name__ =='__main__':
-    draw(Map40, [[5, 16, 37, 12, 34], [8, 14, 16, 37, 12, 17, 10, 24]], ['aquamarine', 'deepskyblue', 'gold'])
+    draw(road_map=Map40, path=[5, 16, 37, 12, 34], colors=['aquamarine', 'gold'], output_file="map40_5_34.svg")
+    draw(road_map=Map40, path=[8, 14, 16, 37, 12, 17, 10, 24], colors=['deepskyblue', 'gold'], output_file="map40_8_24.svg")
